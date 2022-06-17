c56141a131cf618327616e1221f4deaf415c9dd3
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2022-06-17 20:09:37 +0200

    symulator done, czestosc przebywania done, jeszcze sredni czas powrotu to do

--- AA_lab7_zad15.py
+++ AA_lab7_zad15.py
@@ -1,2 +1,27 @@
-p = 20
-q = 20
+import random
+
+p = 0.3
+q = 0.3
+qsize = 0 #liczba osob w kolejce
+stany = [0]*10
+
+for steps in range(1,10000):
+    x = random.random()
+    if x < q:
+        krok = -1  # cofamy się (liczebność kolejki maleje)
+    elif x >= q and x < p+q:
+        krok = 1  # idziemy naprzód (kolejka się zwiększa)
+    else:
+        krok = 0  # nic, kolejka nie zmienia się
+    qsize += krok
+    if qsize < 0:
+        qsize = 0
+    if qsize > len(stany)-1:
+        stany.append(0)
+    stany[qsize] += 1
+
+if len(stany) > 150:
+    print(stany[0:150])
+else:
+    print(stany)
+print('Największy stan kolejki:', len(stany))
